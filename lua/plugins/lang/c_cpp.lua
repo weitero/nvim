38cf6f2a7e659350742a3097d3b4bbe94d6c7ac2
weitero <112836181+weitero@users.noreply.github.com>
2025-09-30 13:08:00 +0100

    config c & cpp

---
+++ lua/plugins/lang/c_cpp.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = { ensure_installed = { "c", "cpp" } },
+  },
+
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = { ensure_installed = { "clangd" } },
+  },
+
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    opts = { ensure_installed = { "clang-format" } },
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = { c = { "clang-format" }, cpp = { "clang-format" } },
+      formatters = { ["clang-format"] = { prepend_args = { "--sort-includes", "--style=google" } } },
+    },
+  },
+}
