3983695084ee10e4eca37db1abc8c5a1ca7a2f1c
weitero <112836181+weitero@users.noreply.github.com>
2025-09-30 13:16:09 +0100

    change of colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,28 +1,10 @@
 return {
   {
-    "rebelot/kanagawa.nvim",
-    opts = {
-      overrides = function(colors)
-        local theme = colors.theme
-        return {
-          NormalFloat = { bg = "none" },
-          FloatBorder = { bg = "none" },
-          FloatTitle = { bg = "none" },
-
-          -- Save an hlgroup with dark background and dimmed foreground
-          -- so that you can use it where your still want darker windows.
-          -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
-          NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
-
-          -- Popular plugins that open floats will link to NormalFloat by default;
-          -- set their background accordingly if you wish to keep them dark and borderless
-          LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-          MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-        }
-      end,
-    },
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
     init = function(_)
-      vim.cmd("colorscheme kanagawa")
+      vim.cmd.colorscheme("catppuccin")
     end,
   },
 
