5a3034e28efe6da81f07bda55e482bfc92d3307c
weitero <112836181+weitero@users.noreply.github.com>
2025-11-15 01:20:38 +0000

    use same settings as vscode

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -23,26 +23,32 @@ return {
         lua_ls = {
           settings = {
             Lua = {
+              codeLens = { enable = true }, -- Enable code lens.
+              hint = { enable = true }, -- Whether inline hints should be enabled or not.
+              hover = { previewFields = 10 }, -- When a table is hovered, its fields will be displayed in the tooltip. This setting limits how many fields can be seen in the tooltip.
               runtime = {
-                -- Tell the language server which version of Lua you're using (most
-                -- likely LuaJIT in the case of Neovim)
-                version = "LuaJIT",
                 -- Tell the language server how to find Lua modules same way as Neovim
                 -- (see `:h lua-module-load`)
                 path = {
+                  "?.lua",
+                  "?/init.lua",
                   "lua/?.lua",
                   "lua/?/init.lua",
-                },
+                }, -- Defines the paths to use when using require.
+                -- Tell the language server which version of Lua you're using (most
+                -- likely LuaJIT in the case of Neovim)
+                version = "LuaJIT", -- The Lua runtime version to use in this environment.
               },
+              type = { castNumberToInteger = true }, -- Whether casting a number to an integer is allowed.
               -- Make the server aware of Neovim runtime files
               workspace = {
-                checkThirdParty = false,
+                checkThirdParty = false, -- Whether addons can be automatically detected and the user can be prompted to enable them.
                 library = {
                   -- Depending on the usage, you might want to add additional paths
                   -- here.
                   "${3rd}/luv/library",
                   "${3rd}/busted/library",
-                },
+                }, -- Used to add library implementation code and definition files to the workspace scope.
               },
             },
           },
