7a9ed73b7f5ddd9e5b1f4bcb64bfc7d645a1abb3
weitero <112836181+weitero@users.noreply.github.com>
2025-11-16 00:30:52 +0000

    formatted code

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,18 +2,25 @@
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
   pattern = "*",
   callback = function()
-    -- how automatic formatting is to be done
     -- https://neovim.io/doc/user/change.html#fo-table
-    vim.opt.formatoptions:remove({
-      "c", -- Auto-wrap comments using 'textwidth', inserting the current comment leader automatically.
-      "r", -- Automatically insert the current comment leader after hitting <Enter> in Insert mode.
-      "o", -- Automatically insert the current comment leader after hitting 'o' or 'O' in Normal mode. In case comment is unwanted in a specific place use CTRL-U to quickly delete it. i_CTRL-U
-    })
+    vim.opt.formatoptions:remove({ "c", "r", "o" })
   end,
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "json", "lua", "r", "sh", "vim", "zsh" },
+  pattern = {
+    "json",
+    "jsonc",
+    "json5",
+    "css",
+    "html",
+    "javascript",
+    "javascriptreact",
+    "lua",
+    "sh",
+    "vim",
+    "zsh",
+  },
   callback = function()
     vim.bo.shiftwidth = 2
     vim.bo.softtabstop = 2
