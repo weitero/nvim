eb7255db308650053c47d56e114cf0bd85e79b1e
weitero <112836181+weitero@users.noreply.github.com>
2025-11-17 15:47:23 +0000

    set CursorColumn same as CursorLine

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,15 +3,27 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
-    init = function()
-      vim.cmd.colorscheme("catppuccin")
-    end,
+    init = function() vim.cmd.colorscheme("catppuccin") end,
     opts = {
       float = {
         transparent = true, -- enable transparent floating windows
       },
       -- let catppuccin automatically detect installed plugins and enable their respective integrations
       auto_integrations = true,
+      highlight_overrides = {
+        all = function(colors)
+          local U = require("catppuccin.utils.colors")
+          return {
+            -- https://github.com/catppuccin/nvim/blob/main/lua/catppuccin/groups/editor.lua
+            CursorColumn = {
+              bg = U.vary_color(
+                { latte = U.lighten(colors.mantle, 0.70, colors.base) },
+                U.darken(colors.surface0, 0.64, colors.base)
+              ),
+            },
+          }
+        end,
+      },
     },
   },
 
