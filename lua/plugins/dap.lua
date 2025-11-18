ad131cabcdbc727b1aefd061d92a3bddffc6cb58
weitero <112836181+weitero@users.noreply.github.com>
2025-11-18 12:59:30 +0000

    tidied up code; moved icons to `config/init.lua`

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,113 +1,66 @@
+local icons = require("config").icons
+
 return {
   {
     "mfussenegger/nvim-dap",
+    -- https://github.com/mfussenegger/nvim-dap/blob/master/doc/dap.txt
+    init = function(_)
+      local sign_define = vim.fn.sign_define
+      sign_define("DapBreakpoint", { text = icons.dap.Breakpoint, texthl = "", linehl = "", numhl = "" })
+      sign_define(
+        "DapBreakpointCondition",
+        { text = icons.dap.BreakpointCondition, texthl = "", linehl = "", numhl = "" }
+      )
+      sign_define("DapLogPoint", { text = icons.dap.LogPoint, texthl = "", linehl = "", numhl = "" })
+      sign_define("DapStopped", { text = icons.dap.Stopped, texthl = "DiagnosticWarn", linehl = "", numhl = "" })
+      sign_define(
+        "DapBreakpointRejected",
+        { text = icons.dap.BreakpointRejected, texthl = "DiagnosticError", linehl = "", numhl = "" }
+      )
+    end,
     keys = {
+      { "<F5>", function() require("dap").continue() end },
+      { "<F10>", function() require("dap").step_over() end },
+      { "<F11>", function() require("dap").step_into() end },
+      { "<F12>", function() require("dap").step_out() end },
+      { "<Leader>b", function() require("dap").toggle_breakpoint() end },
+      { "<Leader>B", function() require("dap").set_breakpoint() end },
       {
-        "<F5>",
-        function()
-          require("dap").continue()
-        end,
+        "<Leader>lp",
+        function() require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: ")) end,
       },
+      { "<Leader>dr", function() require("dap").repl.open() end },
+      { "<Leader>dl", function() require("dap").run_last() end },
+      { "<Leader>dh", function() require("dap.ui.widgets").hover() end, mode = { "n", "v" } },
+      { "<Leader>dp", function() require("dap.ui.widgets").preview() end, mode = { "n", "v" } },
       {
-        "<F10>",
+        "<Leader>ds",
         function()
-          require("dap").step_over()
-        end,
-      },
-      {
-        "<F11>",
-        function()
-          require("dap").step_into()
-        end,
-      },
-      {
-        "<F12>",
-        function()
-          require("dap").step_out()
-        end,
-      },
-      {
-        "<leader>b",
-        function()
-          require("dap").toggle_breakpoint()
-        end,
-      },
-      {
-        "<leader>B",
-        function()
-          require("dap").set_breakpoint()
-        end,
-      },
-      {
-        "<leader>lp",
-        function()
-          require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
-        end,
-      },
-      {
-        "<leader>dr",
-        function()
-          require("dap").repl.open()
-        end,
-      },
-      {
-        "<leader>dl",
-        function()
-          require("dap").run_last()
+          local widgets = require("dap.ui.widgets")
+          widgets.centered_float(widgets.scopes)
         end,
       },
     },
-    init = function(_)
-      local icons = require("config").icons.dap
-      vim.fn.sign_define("DapBreakpoint", { text = icons.Breakpoint, texthl = "", linehl = "", numhl = "" })
-      vim.fn.sign_define(
-        "DapBreakpointCondition",
-        { text = icons.BreakpointCondition, texthl = "", linehl = "", numhl = "" }
-      )
-      vim.fn.sign_define("DapLogPoint", { text = icons.LogPoint, texthl = "", linehl = "", numhl = "" })
-      vim.fn.sign_define(
-        "DapStopped",
-        { text = icons.Stopped[1], texthl = icons.Stopped[2], linehl = icons.Stopped[3], numhl = "" }
-      )
-      vim.fn.sign_define(
-        "DapBreakpointRejected",
-        { text = icons.BreakpointRejected[1], texthl = icons.BreakpointRejected[2], linehl = "", numhl = "" }
-      )
-    end,
   },
 
   {
     "rcarriga/nvim-dap-ui",
+    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
     init = function(_)
       local dap, dapui = require("dap"), require("dapui")
-      dap.listeners.before.attach.dapui_config = function()
-        dapui.open()
-      end
-      dap.listeners.before.launch.dapui_config = function()
-        dapui.open()
-      end
-      dap.listeners.before.event_terminated.dapui_config = function()
-        dapui.close()
-      end
-      dap.listeners.before.event_exited.dapui_config = function()
-        dapui.close()
-      end
+      dap.listeners.before.attach.dapui_config = function() dapui.open() end
+      dap.listeners.before.launch.dapui_config = function() dapui.open() end
+      dap.listeners.before.event_terminated.dapui_config = function() dapui.close() end
+      dap.listeners.before.event_exited.dapui_config = function() dapui.close() end
     end,
-    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
     opts = {
-      icons = { expanded = "", collapsed = "", current_frame = "" },
       controls = {
-        icons = {
-          pause = "",
-          play = "",
-          step_into = "",
-          step_over = "",
-          step_out = "",
-          step_back = "",
-          run_last = "",
-          terminate = "",
-          disconnect = "",
-        },
+        icons = icons.dapui.controls,
+      },
+      icons = {
+        expanded = icons.dapui.expanded,
+        collapsed = icons.dapui.collapsed,
+        current_frame = icons.dapui.current_frame,
       },
     },
   },
@@ -115,8 +68,6 @@ return {
   { "theHamsta/nvim-dap-virtual-text", opts = {} },
 
   { "LiadOz/nvim-dap-repl-highlights", opts = {} },
-
-  { "WhoIsSethDaniel/mason-tool-installer.nvim" },
 
   {
     "nvim-treesitter/nvim-treesitter",
