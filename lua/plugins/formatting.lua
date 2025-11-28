0a10653f1983f5c7effb1fc0c2c5b330f289b40f
weitero <112836181+weitero@users.noreply.github.com>
2025-11-28 16:16:23 +0000

    Remove stylua; use luals instead

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -29,10 +29,6 @@ return {
     ---@module "conform"
     ---@type conform.setupOpts
     opts = {
-      -- Define your formatters
-      formatters_by_ft = {
-        -- lua = { "stylua" },
-      },
       -- Set default options
       default_format_opts = {
         lsp_format = "fallback",
