e8c2b1f2e69ded3d85beabdac9df4fc113b5a8f2
weitero <112836181+weitero@users.noreply.github.com>
2025-11-28 00:40:50 +0000

    Modify Lua formatting configuration by commenting out the default Lua formatter

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -31,10 +31,8 @@ return {
     opts = {
       -- Define your formatters
       formatters_by_ft = {
-        lua = { "stylua" },
-        -- lua = { "lua_ls" },
+        -- lua = { "stylua" },
       },
-      -- formatters = { lua_ls = { command = 'lua-language-server', args = {} } },
       -- Set default options
       default_format_opts = {
         lsp_format = "fallback",
