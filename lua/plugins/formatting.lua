952c4b52d1594fe100c4320b37590ec00098f4da
weitero <112836181+weitero@users.noreply.github.com>
2025-11-29 01:32:50 +0000

    Wrap long function call arguments for better readability in formatting configuration

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -58,7 +58,8 @@ return {
               vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
           range = { start = { args.line1, 0 }, ["end"] = { args.line2, end_line:len() } }
         end
-        require("conform").format({ async = true, lsp_format = "fallback", range = range })
+        require("conform").format({ async = true, lsp_format = "fallback", range =
+        range })
       end, { range = true })
 
       -- Create user commands to quickly enable/disable autoformatting
