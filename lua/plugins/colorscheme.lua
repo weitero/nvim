0cacf3139bcb52c18772d300d3ad453e04da46a1
weitero <112836181+weitero@users.noreply.github.com>
2026-01-16 01:10:19 +0000

    refactor: update colorscheme configuration for clarity and consistency

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,24 +1,25 @@
+local catppuccin_colors = require("catppuccin.utils.colors")
+
 return {
   {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
-    init = function() vim.cmd.colorscheme("catppuccin") end,
+    config = function()
+      vim.cmd.colorscheme("catppuccin")
+    end,
     opts = {
-      float = {
-        transparent = true, -- enable transparent floating windows
-      },
-      -- let catppuccin automatically detect installed plugins and enable their respective integrations
+      float = { transparent = true },
       auto_integrations = true,
       highlight_overrides = {
         all = function(colors)
-          local U = require("catppuccin.utils.colors")
           return {
+            -- Match upstream CursorColumn, but tweak for better contrast
             -- https://github.com/catppuccin/nvim/blob/main/lua/catppuccin/groups/editor.lua
             CursorColumn = {
-              bg = U.vary_color(
-                { latte = U.lighten(colors.mantle, 0.70, colors.base) },
-                U.darken(colors.surface0, 0.64, colors.base)
+              bg = catppuccin_colors.vary_color(
+                { latte = catppuccin_colors.lighten(colors.mantle, 0.70, colors.base) },
+                catppuccin_colors.darken(colors.surface0, 0.64, colors.base)
               ),
             },
           }
@@ -27,13 +28,5 @@ return {
     },
   },
 
-  -- Lua
-  {
-    "f-person/auto-dark-mode.nvim",
-    opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    },
-  },
+  { "f-person/auto-dark-mode.nvim", opts = {} },
 }
