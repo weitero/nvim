8b085b4a51be81d147eb060cb54f487d29dea88b
weitero <112836181+weitero@users.noreply.github.com>
2026-01-17 22:51:42 +0000

    feat: add Go language support with Treesitter and LSP configurations

---
+++ lua/plugins/lang/go.lua
@@ -0,0 +1,40 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = { ensure_installed = {
+      "go",
+      "gomod",
+      "gosum",
+      "gowork",
+    } },
+  },
+
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = { ensure_installed = { "gopls" } },
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      settings = {
+        gopls = {
+          gofumpt = true,
+          usePlaceholder = true,
+          analyses = { unusedparams = true },
+          staticcheck = true,
+          hints = {
+            assignVariableTypes = true,
+            compositeLiteralFields = true,
+            compositeLiteralTypes = true,
+            constantValues = true,
+            functionTypeParameters = true,
+            ignoredError = true,
+            parameterNames = true,
+            rangeVariableTypes = true,
+          },
+        },
+      },
+    },
+  },
+}
