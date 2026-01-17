c2b110df5f51922ac3a782762e03f78684b1052d
weitero <112836181+weitero@users.noreply.github.com>
2026-01-17 17:48:05 +0000

    feat: update css.lua to include tailwindcss and use prettier for formatting

--- lua/plugins/lang/css.lua
+++ lua/plugins/lang/css.lua
@@ -6,16 +6,42 @@ return {
 
   {
     "mason-org/mason-lspconfig.nvim",
-    opts = { ensure_installed = { "cssls" } },
+    opts = {
+      ensure_installed = {
+        "cssls",
+        "tailwindcss",
+      },
+    },
   },
 
   {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
-    opts = { ensure_installed = { "biome" } },
+    opts = { ensure_installed = { "prettier" } },
   },
 
   {
     "stevearc/conform.nvim",
-    opts = { formatters_by_ft = { css = { "biome" } } },
+    opts = {
+      formatters_by_ft = { css = { "prettier" } },
+      formatters = {
+        prettier = {
+          prepend_args = {
+            "--experimental-operator-position",
+            "start",
+            "--print-width",
+            "120",
+            "--single-quote",
+            "--quote-props",
+            "consistent",
+            "--trailing-comma",
+            "es5",
+            "--check-ignore-pragma",
+            "--prose-wrap",
+            "always",
+            "--vue-indent-script-and-style",
+          },
+        },
+      },
+    },
   },
 }
