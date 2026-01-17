c6fa921040b61ecada6ec3ac77bb7887b28057f5
weitero <112836181+weitero@users.noreply.github.com>
2026-01-17 22:54:58 +0000

    feat: update html.lua to use prettier for formatting and ensure installation

--- lua/plugins/lang/html.lua
+++ lua/plugins/lang/html.lua
@@ -11,11 +11,31 @@ return {
 
   {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
-    opts = { ensure_installed = { "biome" } },
+    opts = { ensure_installed = { "prettier" } },
   },
 
   {
     "stevearc/conform.nvim",
-    opts = { formatters_by_ft = { html = { "biome" } } },
+    opts = {
+      formatters_by_ft = { html = { "prettier" } },
+      formatters = {
+        prettier = {
+          prepend_args = {
+            "--experimental-operator-position",
+            "start",
+            "--print-width",
+            "120",
+            "--quote-props",
+            "consistent",
+            "--trailing-comma",
+            "es5",
+            "--check-ignore-pragma",
+            "--prose-wrap",
+            "always",
+            "--vue-indent-script-and-style",
+          },
+        },
+      },
+    },
   },
 }
