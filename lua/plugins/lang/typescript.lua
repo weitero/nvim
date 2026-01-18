c6ee81bf64ee0e91c8f2bb218d0e054f43ab86ae
weitero <112836181+weitero@users.noreply.github.com>
2026-01-18 02:00:56 +0000

    feat: add TypeScript language support with Treesitter and LSP configuration

---
+++ lua/plugins/lang/typescript.lua
@@ -0,0 +1,56 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "tsx",
+        "typescript",
+      },
+    },
+  },
+
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = {
+        "eslint",
+        "tailwindcss",
+        "vtsls",
+      },
+    },
+  },
+
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    opts = { ensure_installed = { "prettier" } },
+  },
+
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        typescript = { "prettier" },
+        typescriptreact = { "prettier" },
+      },
+      formatters = {
+        prettier = {
+          prepend_args = {
+            "--experimental-operator-position",
+            "start",
+            "--print-width",
+            "120",
+            "--single-quote",
+            "--quote-props",
+            "consistent",
+            "--trailing-comma",
+            "es5",
+            "--check-ignore-pragma",
+            "--prose-wrap",
+            "always",
+            "--vue-indent-script-and-style",
+          },
+        },
+      },
+    },
+  },
+}
