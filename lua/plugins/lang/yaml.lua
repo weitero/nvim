bc40984e4b3fc99252e104167a2f465652efa475
weitero <112836181+weitero@users.noreply.github.com>
2026-01-18 02:02:30 +0000

    feat: add YAML language support with Treesitter and LSP configuration

--- lua/plugins/lang/yaml.lua
+++ lua/plugins/lang/yaml.lua
@@ -1,7 +1,11 @@
 return {
-
   {
     "nvim-treesitter/nvim-treesitter",
     opts = { ensure_installed = { "yaml" } },
+  },
+
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = { ensure_installed = { "yamlls" } },
   },
 }
