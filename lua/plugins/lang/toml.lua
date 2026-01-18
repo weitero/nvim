ed413a15b9c520cac81d8984463436e9e6456faf
weitero <112836181+weitero@users.noreply.github.com>
2026-01-18 01:58:23 +0000

    feat: update TOML language support by replacing 'taplo' with 'tombi' for LSP and formatting

--- lua/plugins/lang/toml.lua
+++ lua/plugins/lang/toml.lua
@@ -6,36 +6,11 @@ return {
 
   {
     "mason-org/mason-lspconfig.nvim",
-    opts = { ensure_installed = { "taplo" } },
-  },
-
-  {
-    "neovim/nvim-lspconfig",
-    opts = { servers = { taplo = { settings = { root_markers = { "starship.toml" }, }, }, }, },
+    opts = { ensure_installed = { "tombi" } },
   },
 
   {
     "stevearc/conform.nvim",
-    opts = {
-      formatters_by_ft = { toml = { "taplo" }, },
-      formatters = {
-        taplo = {
-          args = function ()
-            if
-                require("config").find_config({ ".taplo.toml", "taplo.toml" })
-            then
-              return require("conform.formatters.taplo").args
-            end
-            return {
-              "format",
-              "-c", vim.fn.stdpath('data') .. "/formatter-configs/taplo.toml",
-              "--stdin-filepath",
-              "$FILENAME",
-              "-",
-            }
-          end,
-        },
-      },
-    },
+    opts = { formatters_by_ft = { toml = { "tombi" } } },
   },
 }
