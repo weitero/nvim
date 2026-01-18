52500f721244cdf9ab7433fbac646e102e93b49c
weitero <112836181+weitero@users.noreply.github.com>
2026-01-18 19:39:27 +0000

    feat: add zsh language support with LSP and formatting configuration

--- lua/plugins/lang/zsh.lua
+++ lua/plugins/lang/zsh.lua
@@ -1,6 +1,27 @@
 return {
   {
-    "zdharma-continuum/zinit-vim-syntax",
-    ft = { "zsh" },
+    "mason-org/mason-lspconfig.nvim",
+    opts = { ensure_installed = { "bashls" } },
+  },
+
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    opts = { ensure_installed = { "beautysh" } },
+  },
+
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = { zsh = { "beautysh" } },
+      formatters = {
+        beautysh = {
+          prepend_args = {
+            "-i",
+            "2",
+            "-b",
+          },
+        },
+      },
+    },
   },
 }
