b35a8eb861b82c0b181f75d02083a7a0b94adf0c
weitero <112836181+weitero@users.noreply.github.com>
2026-01-19 00:24:57 +0000

    fix: correct jinja filetype extension and improve LSP configuration formatting

--- lua/plugins/lang/jinja.lua
+++ lua/plugins/lang/jinja.lua
@@ -1,8 +1,8 @@
 vim.filetype.add({
   extension = {
+    j2 = "jinja",
     jinja = "jinja",
     jinja2 = "jinja",
-    j2 = "jinja",
   },
 })
 
@@ -29,7 +29,19 @@ return {
 
   {
     "neovim/nvim-lspconfig",
-    opts = { servers = { jinja_lsp = { settings = { filetypes = { "jinja" } } } } },
+    opts = {
+      servers = {
+        jinja_lsp = {
+          settings = {
+            filetypes = { "jinja" },
+            settings = {
+              templates = "./templates",
+              backend = "./src",
+            },
+          },
+        },
+      },
+    },
   },
 
   {
