01d04c6f3ba6d355dc000e0bf0915b20acd9dd45
weitero <112836181+weitero@users.noreply.github.com>
2026-01-20 01:02:40 +0000

    feat: add YAML filetype support and configure LSP settings

--- lua/plugins/lang/yaml.lua
+++ lua/plugins/lang/yaml.lua
@@ -1,3 +1,5 @@
+vim.filetype.add({ filename = { ["_clang-format"] = "yaml" } })
+
 return {
   {
     "nvim-treesitter/nvim-treesitter",
@@ -7,5 +9,32 @@ return {
   {
     "mason-org/mason-lspconfig.nvim",
     opts = { ensure_installed = { "yamlls" } },
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        yamlls = {
+          settings = {
+            yaml = {
+              format = {
+                proseWrap = "always",
+                printWidth = 120,
+              },
+              schemas = { ["https://www.schemastore.org/clang-format-21.x.json"] = "_clang-format" },
+              keyOrdering = true,
+            },
+          },
+        },
+      },
+    },
+  },
+
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = { yaml = { lsp_format = "fallback" } },
+    },
   },
 }
