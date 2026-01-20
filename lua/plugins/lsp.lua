964099ac011c9302dc11692bfa5af6839784bb70
weitero <112836181+weitero@users.noreply.github.com>
2026-01-20 01:17:24 +0000

    fix: improve formatting and organization of Lua LSP settings

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -12,13 +12,39 @@ return {
             -- https://luals.github.io/wiki/settings/
             Lua = {
               codeLens = { enable = true },
-              completion = { callSnippet = "Both", keywordSnippet = "Both" },
+              completion = {
+                callSnippet = "Both",
+                keywordSnippet = "Both",
+              },
               diagnostics = { neededFileStatus = { ["codestyle-check"] = "Any" } },
-              format = { defaultConfig = { indent_size = "2", continuation_indent = "2" }, enable = false },
-              hint = { enable = true },
-              runtime = { path = { "?.lua", "?/init.lua", "lua/?.lua", "lua/?/init.lua" }, version = "LuaJIT" },
+              format = {
+                defaultConfig = {
+                  indent_size = "2",
+                  continuation_indent = "2",
+                },
+                enable = false,
+              },
+              hint = {
+                enable = true,
+                setType = true,
+              },
+              runtime = {
+                path = {
+                  "?.lua",
+                  "?/init.lua",
+                  "lua/?.lua",
+                  "lua/?/init.lua",
+                },
+                version = "LuaJIT",
+              },
               telemetry = { enable = false },
-              workspace = { checkThirdParty = false, library = { "${3rd}/busted/library", "${3rd}/luv/library" } },
+              workspace = {
+                checkThirdParty = false,
+                library = {
+                  "${3rd}/busted/library",
+                  "${3rd}/luv/library",
+                },
+              },
             },
           },
         },
