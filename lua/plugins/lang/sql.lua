26b2cca2045088d50c7bc5a6f71dcb8d0c65a34f
weitero <112836181+weitero@users.noreply.github.com>
2026-01-25 22:38:54 +0000

    fix: improve formatting and organization of sql.lua configuration

--- lua/plugins/lang/sql.lua
+++ lua/plugins/lang/sql.lua
@@ -1,21 +1,48 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    opts = { ensure_installed = { "sql" } },
+    opts = {
+      ensure_installed = {
+        "sql",
+      },
+    },
   },
 
   {
     "mason-org/mason-lspconfig.nvim",
-    opts = { ensure_installed = { "sqls" } },
+    opts = {
+      ensure_installed = {
+        "sqls",
+      },
+    },
   },
 
   {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
-    opts = { ensure_installed = { "sql-formatter" } },
+    opts = {
+      ensure_installed = {
+        "sql-formatter",
+      },
+    },
   },
 
   {
     "stevearc/conform.nvim",
-    opts = { formatters_by_ft = { sql = { "sql_formatter" } } },
+    opts = {
+      formatters_by_ft = {
+        sql = {
+          "sql_formatter",
+          lsp_format = "never",
+        },
+      },
+      formatters = {
+        sql_formatter = {
+          prepend_args = {
+            "-l",
+            "sqlite",
+          },
+        },
+      },
+    },
   },
 }
