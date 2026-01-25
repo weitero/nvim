b2134d07f3efa1f33d3d3c486aa65c2c71a36440
weitero <112836181+weitero@users.noreply.github.com>
2026-01-25 21:56:08 +0000

    fix: improve formatting and organization of beancount.lua configuration

--- lua/plugins/lang/beancount.lua
+++ lua/plugins/lang/beancount.lua
@@ -1,36 +1,37 @@
 vim.filetype.add({
   extension = {
-    beancount = "beancount",
     bean = "beancount",
+    beancount = "beancount",
   },
 })
 
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    opts = { ensure_installed = { "beancount" } },
+    opts = {
+      ensure_installed = {
+        "beancount",
+      },
+    },
   },
 
   {
     "mason-org/mason-lspconfig.nvim",
-    opts = { ensure_installed = { "beancount" } },
-  },
-
-  {
-    "neovim/nvim-lspconfig",
     opts = {
-      init_options = {
-        formatting = {
-          prefix_width = 30,
-          currency_column = 60,
-          number_currency_spacing = 1,
-        },
+      ensure_installed = {
+        "beancount",
       },
     },
   },
 
   {
     "stevearc/conform.nvim",
-    opts = { formatters_by_ft = { beancount = { "bean-format" } } },
+    opts = {
+      formatters_by_ft = {
+        beancount = {
+          lsp_format = "prefer",
+        },
+      },
+    },
   },
 }
