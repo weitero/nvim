a3070e82d61a72e8be374eacafb39e190b0c5235
weitero <112836181+weitero@users.noreply.github.com>
2026-01-25 22:06:46 +0000

    fix: improve formatting and organization of css.lua configuration

--- lua/plugins/lang/css.lua
+++ lua/plugins/lang/css.lua
@@ -1,7 +1,12 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    opts = { ensure_installed = { "css" } },
+    opts = {
+      ensure_installed = {
+        "css",
+        "scss",
+      },
+    },
   },
 
   {
@@ -16,30 +21,20 @@ return {
 
   {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
-    opts = { ensure_installed = { "prettier" } },
+    opts = {
+      ensure_installed = {
+        "prettier",
+      },
+    },
   },
 
   {
     "stevearc/conform.nvim",
     opts = {
-      formatters_by_ft = { css = { "prettier" } },
-      formatters = {
-        prettier = {
-          prepend_args = {
-            "--experimental-operator-position",
-            "start",
-            "--print-width",
-            "120",
-            "--single-quote",
-            "--quote-props",
-            "consistent",
-            "--trailing-comma",
-            "es5",
-            "--check-ignore-pragma",
-            "--prose-wrap",
-            "always",
-            "--vue-indent-script-and-style",
-          },
+      formatters_by_ft = {
+        css = {
+          "prettier",
+          lsp_format = "never",
         },
       },
     },
