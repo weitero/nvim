60c8e084e107a172022a135c68b8dfca953fe6ee
weitero <112836181+weitero@users.noreply.github.com>
2026-01-25 21:50:42 +0000

    fix: improve formatting and organization of awk.lua configuration

--- lua/plugins/lang/awk.lua
+++ lua/plugins/lang/awk.lua
@@ -1,11 +1,40 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    opts = { ensure_installed = { "awk" } },
+    opts = {
+      ensure_installed = {
+        "awk",
+      },
+    },
   },
 
   {
     "mason-org/mason-lspconfig.nvim",
-    opts = { ensure_installed = { "awk_ls" } },
+    opts = {
+      ensure_installed = {
+        "awk_ls",
+      },
+    },
+  },
+
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    opts = {
+      ensure_installed = {
+        "prettier",
+      },
+    },
+  },
+
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        awk = {
+          "prettier",
+          lsp_format = "never",
+        },
+      },
+    },
   },
 }
