23da70355ca17ac4ea31f3e33cdc83e451d6f146
weitero <112836181+weitero@users.noreply.github.com>
2026-01-25 22:13:54 +0000

    fix: improve formatting and organization of emmet.lua configuration

--- lua/plugins/lang/emmet.lua
+++ lua/plugins/lang/emmet.lua
@@ -1,6 +1,28 @@
 return {
   {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "astro",
+        "css",
+        "html",
+        "htmldjango",
+        "pug",
+        "scss",
+        "svelte",
+        "templ",
+        "tsx",
+        "vue",
+      },
+    },
+  },
+
+  {
     "mason-org/mason-lspconfig.nvim",
-    opts = { ensure_installed = { "emmet_language_server" } },
+    opts = {
+      ensure_installed = {
+        "emmet_language_server",
+      },
+    },
   },
 }
