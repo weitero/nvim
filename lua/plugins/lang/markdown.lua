030ec2d0b66cb1013031d74095c6c0e9ae596365
weitero <112836181+weitero@users.noreply.github.com>
2026-01-25 22:35:16 +0000

    fix: improve formatting and organization of markdown.lua configuration

--- lua/plugins/lang/markdown.lua
+++ lua/plugins/lang/markdown.lua
@@ -1,9 +1,32 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    opts = { ensure_installed = {
-      "markdown",
-      "markdown_inline",
-    } },
+    opts = {
+      ensure_installed = {
+        "markdown",
+        "markdown_inline",
+      },
+    },
+  },
+
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    opts = {
+      ensure_installed = {
+        "prettier",
+      },
+    },
+  },
+
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        markdown = {
+          "prettier",
+          lsp_format = "never",
+        },
+      },
+    },
   },
 }
