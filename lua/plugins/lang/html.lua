f4f2a9318e91a2ea7e03832cb3e7f528265745c9
weitero <112836181+weitero@users.noreply.github.com>
2026-01-25 22:16:52 +0000

    fix: improve formatting and organization of html.lua configuration

--- lua/plugins/lang/html.lua
+++ lua/plugins/lang/html.lua
@@ -1,39 +1,38 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    opts = { ensure_installed = { "html" } },
+    opts = {
+      ensure_installed = {
+        "html",
+      },
+    },
   },
 
   {
     "mason-org/mason-lspconfig.nvim",
-    opts = { ensure_installed = { "html" } },
+    opts = {
+      ensure_installed = {
+        "html",
+      },
+    },
   },
 
   {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
-    opts = { ensure_installed = { "prettier" } },
+    opts = {
+      ensure_installed = {
+        "prettier",
+      },
+    },
   },
 
   {
     "stevearc/conform.nvim",
     opts = {
-      formatters_by_ft = { html = { "prettier" } },
-      formatters = {
-        prettier = {
-          prepend_args = {
-            "--experimental-operator-position",
-            "start",
-            "--print-width",
-            "120",
-            "--quote-props",
-            "consistent",
-            "--trailing-comma",
-            "es5",
-            "--check-ignore-pragma",
-            "--prose-wrap",
-            "always",
-            "--vue-indent-script-and-style",
-          },
+      formatters_by_ft = {
+        html = {
+          "prettier",
+          lsp_format = "never",
         },
       },
     },
