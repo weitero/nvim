b91606063c1ee6bbc2c6a626c00cbc74ddcf93ad
weitero <112836181+weitero@users.noreply.github.com>
2026-01-25 22:37:57 +0000

    fix: improve formatting and organization of rust.lua configuration

--- lua/plugins/lang/rust.lua
+++ lua/plugins/lang/rust.lua
@@ -1,11 +1,61 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    opts = { ensure_installed = { "rust" } },
+    opts = {
+      ensure_installed = {
+        "rust",
+      },
+    },
   },
 
   {
     "mason-org/mason-lspconfig.nvim",
-    opts = { ensure_installed = { "rust_analyzer" } },
+    opts = {
+      ensure_installed = {
+        "rust_analyzer",
+      },
+    },
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        rust_analyzer = {
+          settings = {
+            ["rust-analyzer"] = {
+              completion = { fullFunctionSignatures = { enable = true } },
+              diagnostics = { styleLints = { enable = true } },
+              hover = { actions = { references = { enable = true } } },
+              inlayHints = {
+                bindingModeHints = { enable = true },
+                closureCaptureHints = { enable = true },
+                genericParameterHints = {
+                  lifetime = { enable = true },
+                  type = { enable = true },
+                },
+                implicitDrops = { enable = true },
+                implicitSizedBoundHints = { enable = true },
+                parameterHints = {
+                  missingArguments = { enable = true },
+                  rangeExclusiveHints = { enable = true },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        rust = {
+          lsp_format = "prefer",
+        },
+      },
+    },
   },
 }
