87cfa244a11746166b3a512d49d6b8e81d4f456b
weitero <112836181+weitero@users.noreply.github.com>
2026-01-31 18:18:21 +0000

    fix: reorganize formatting plugin configuration for clarity

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -9,24 +9,6 @@ end
 return {
   {
     "stevearc/conform.nvim",
-
-    init = function()
-      -- If you want the formatexpr, here is the place to set it
-      vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-    end,
-
-    -- This will provide type hinting with LuaLS
-    --- @module "conform"
-    --- @type conform.setupOpts
-    opts = {
-      -- Map of filetype to formatters
-      formatters_by_ft = { lua = { "stylua", lsp_format = "fallback" } },
-      default_format_opts = { lsp_format = "fallback" },
-      format_on_save = { timeout_ms = 500, lsp_format = "fallback" },
-      format_after_save = { lsp_format = "fallback" },
-      formatters = { stylua = { prepend_args = { "--verify" } } },
-    },
-
     -- Here is the recommended config for lazy-loading using lazy.nvim
     event = { "BufWritePre" },
     cmd = { "ConformInfo" },
@@ -42,6 +24,21 @@ return {
         desc = "Format code",
       },
     },
+    -- This will provide type hinting with LuaLS
+    --- @module "conform"
+    --- @type conform.setupOpts
+    opts = {
+      -- Map of filetype to formatters
+      formatters_by_ft = { lua = { "stylua" } },
+      formatters = { stylua = { prepend_args = { "--verify" } } },
+      default_format_opts = { lsp_format = "fallback" },
+      format_on_save = { timeout_ms = 500, lsp_format = "fallback" },
+      format_after_save = { lsp_format = "fallback" },
+    },
+    init = function()
+      -- If you want the formatexpr, here is the place to set it
+      vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+    end,
   },
 
   {
