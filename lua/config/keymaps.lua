a3d26ad83af2f79e2cbe4a1c9147f74113a351db
weitero <112836181+weitero@users.noreply.github.com>
2026-01-31 18:17:44 +0000

    fix: enhance keymaps for better navigation and diagnostics

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,14 +1,37 @@
 local map = vim.keymap.set
 
+--
+-- https://www.lazyvim.org/configuration/general#keymaps
+--
+
 -- better up/down
-map({ "n", "x" }, "<Down>", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
-map({ "n", "x" }, "<Up>", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
 map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
+map({ "n", "x" }, "<Down>", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
 map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
+map({ "n", "x" }, "<Up>", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
+
+-- https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
+map("n", "n", "'Nn'[v:searchforward].'zv'", { expr = true, desc = "Next Search Result" })
+map("x", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next Search Result" })
+map("o", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next Search Result" })
+map("n", "N", "'nN'[v:searchforward].'zv'", { expr = true, desc = "Prev Search Result" })
+map("x", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev Search Result" })
+map("o", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev Search Result" })
 
 -- better indenting
-map("v", "<", "<gv")
-map("v", ">", ">gv")
+map("x", "<", "<gv")
+map("x", ">", ">gv")
+
+-- quickfix list
+map("n", "<leader>xq", function()
+  local success, err = pcall(vim.fn.getqflist({ winid = 0 }).winid ~= 0 and vim.cmd.cclose or vim.cmd.copen)
+  if not success and err then
+    vim.notify(err, vim.log.levels.ERROR)
+  end
+end, { desc = "Quickfix List" })
+
+map("n", "[q", vim.cmd.cprev, { desc = "Previous Quickfix" })
+map("n", "]q", vim.cmd.cnext, { desc = "Next Quickfix" })
 
 -- diagnostics
 local diagnostic_goto = function(count, severity)
@@ -18,7 +41,12 @@ local diagnostic_goto = function(count, severity)
   end
 end
 map("n", "<leader>cd", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
-map("n", "[e", diagnostic_goto(-1, "ERROR"), { desc = "Prev Error" })
-map("n", "[w", diagnostic_goto(-1, "WARN"), { desc = "Prev Warning" })
 map("n", "]e", diagnostic_goto(1, "ERROR"), { desc = "Next Error" })
+map("n", "[e", diagnostic_goto(-1, "ERROR"), { desc = "Prev Error" })
 map("n", "]w", diagnostic_goto(1, "WARN"), { desc = "Next Warning" })
+map("n", "[w", diagnostic_goto(-1, "WARN"), { desc = "Prev Warning" })
+
+-- windows
+map("n", "<leader>-", "<C-W>s", { desc = "Split Window Below", remap = true })
+map("n", "<leader>|", "<C-W>v", { desc = "Split Window Right", remap = true })
+map("n", "<leader>wd", "<C-W>c", { desc = "Delete Window", remap = true })
