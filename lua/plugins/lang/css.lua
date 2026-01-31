ba4159e94d1892129c6c9a4e3cd89b1d3a4523a6
weitero <112836181+weitero@users.noreply.github.com>
2026-01-31 05:28:03 +0000

    fix: update CSS configuration by replacing prettier with biome

--- lua/plugins/lang/css.lua
+++ lua/plugins/lang/css.lua
@@ -23,7 +23,7 @@ return {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
     opts = {
       ensure_installed = {
-        "prettier",
+        "biome",
       },
     },
   },
@@ -33,8 +33,7 @@ return {
     opts = {
       formatters_by_ft = {
         css = {
-          "prettier",
-          lsp_format = "never",
+          "biome",
         },
       },
     },
