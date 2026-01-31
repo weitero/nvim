da15e50e14e9410801210ec04c21cf016132bab6
weitero <112836181+weitero@users.noreply.github.com>
2026-01-31 18:18:30 +0000

    fix: streamline autocmd callback and refine event triggers in lint configuration

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -15,14 +15,13 @@ return {
       local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
       vim.api.nvim_create_autocmd(opts.events, {
         group = lint_augroup,
-        callback = function()
-          lint.try_lint()
-        end,
+        callback = function() lint.try_lint() end,
       })
     end,
     event = {
-      "BufNewFile",
-      "BufReadPre",
+      "BufWritePost",
+      "InsertLeave",
+      "TextChanged",
     },
   },
 }
