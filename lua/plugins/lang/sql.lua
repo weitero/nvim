835f7c3d4fadda6cac0b21e54b4d65c5115c6d1c
weitero <112836181+weitero@users.noreply.github.com>
2026-01-31 01:45:22 +0000

    feat: enhance SQL support with additional dependencies and configuration

--- lua/plugins/lang/sql.lua
+++ lua/plugins/lang/sql.lua
@@ -22,6 +22,52 @@ return {
     opts = {
       ensure_installed = {
         "sql-formatter",
+        "sqlfluff",
+      },
+    },
+  },
+
+  -- Configuration using lazy.nvim with vim-dadbod-ui
+  {
+    "kristijanhusak/vim-dadbod-ui",
+    dependencies = {
+      { "tpope/vim-dadbod", lazy = true },
+      { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+    },
+    cmd = {
+      "DBUI",
+      "DBUIToggle",
+      "DBUIAddConnection",
+      "DBUIFindBuffer",
+    },
+    init = function()
+      -- Your DBUI configuration
+      vim.g.db_ui_use_nerd_fonts = 1
+    end,
+  },
+
+  { -- optional saghen/blink.cmp completion source
+    "saghen/blink.cmp",
+    opts = {
+      sources = {
+        per_filetype = {
+          sql = { inherit_defaults = true, "dadbod" },
+        },
+        -- add vim-dadbod-completion to your completion providers
+        providers = {
+          dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
+        },
+      },
+    },
+  },
+
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters_by_ft = {
+        sql = {
+          "sqlfluff",
+        },
       },
     },
   },
@@ -32,15 +78,6 @@ return {
       formatters_by_ft = {
         sql = {
           "sql_formatter",
-          lsp_format = "never",
-        },
-      },
-      formatters = {
-        sql_formatter = {
-          prepend_args = {
-            "-l",
-            "sqlite",
-          },
         },
       },
     },
