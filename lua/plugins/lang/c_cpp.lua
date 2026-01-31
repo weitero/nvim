062bab863a7f43a1216b7d079a6e5b13e4af09f6
weitero <112836181+weitero@users.noreply.github.com>
2026-01-31 05:29:27 +0000

    fix: remove unused clang-format and conform configurations from C/C++ setup

--- lua/plugins/lang/c_cpp.lua
+++ lua/plugins/lang/c_cpp.lua
@@ -19,49 +19,4 @@ return {
       },
     },
   },
-
-  {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    opts = {
-      ensure_installed = {
-        "clang-format",
-      },
-    },
-  },
-
-  {
-    "stevearc/conform.nvim",
-    opts = {
-      formatters_by_ft = {
-        c = {
-          "clang-format",
-          lsp_format = "never",
-        },
-        cpp = {
-          "clang-format",
-          lsp_format = "never",
-        },
-      },
-      formatters = {
-        ["clang-format"] = {
-          prepend_args = {
-            "--sort-includes",
-            "--style={ \z
-              AccessModifierOffset: -4, \z
-              AllowShortIfStatementsOnASingleLine: Never, \z
-              BasedOnStyle: LLVM, \z
-              BreakBeforeBraces: Allman, \z
-              ColumnLimit: 0, \z
-              FixNamespaceComments: false, \z
-              IndentCaseLabels: false, \z
-              IndentWidth: 4, \z
-              NamespaceIndentation: All, \z
-              TabWidth: 4, \z
-              UseTab: Never, \z
-            }",
-          },
-        },
-      },
-    },
-  },
 }
