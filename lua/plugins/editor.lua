e0a88d319e309c566ca25c613c9f4bd3b79ec445
weitero <112836181+weitero@users.noreply.github.com>
2026-02-08 20:31:00 +0000

    feat: add telescope.nvim plugin with key mappings for file searching

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -2,6 +2,23 @@ local icons = require("config").icons
 
 return {
   {
+    "nvim-telescope/telescope.nvim",
+    version = "*",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      -- optional but recommended
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    },
+    config = function(_, opts)
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+      vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+    end,
+  },
+
+  {
     "nvim-mini/mini.hipatterns",
     version = false,
     config = function(_, opts)
