02d818b7cd4cff4c9c0c4b9be19c4df4db0dd8f2
weitero <112836181+weitero@users.noreply.github.com>
2026-02-26 16:54:16 +0000

    feat: add configuration for texlab and latexindent support in nvim-lspconfig

--- lua/plugins/lang/tex.lua
+++ lua/plugins/lang/tex.lua
@@ -8,4 +8,53 @@ return {
       },
     },
   },
+
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = {
+        "texlab",
+      },
+    },
+  },
+
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    opts = {
+      ensure_installed = {
+        "latexindent",
+      },
+    },
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        texlab = {
+          settings = {
+            texlab = {
+              chktex = {
+                onOpenAndSave = true,
+                onEdit = true,
+              },
+              latexindent = {
+                modifyLineBreaks = true,
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+
+  {
+    "lervag/vimtex",
+    lazy = false, -- we don't want to lazy load VimTeX
+    -- tag = "v2.15", -- uncomment to pin to a specific release
+    init = function()
+      -- VimTeX configuration goes here, e.g.
+      vim.g.vimtex_view_method = "zathura"
+    end,
+  },
 }
