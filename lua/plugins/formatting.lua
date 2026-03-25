df11a411bea8123281d7c1468e6c5aa2eb68d54b
weitero <112836181+weitero@users.noreply.github.com>
2026-03-25 22:32:47 +0000

    refactor: disable format_on_save and format_after_save options in formatting configuration

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -32,8 +32,10 @@ return {
       formatters_by_ft = { lua = { "stylua" } },
       formatters = { stylua = { prepend_args = { "--verify" } } },
       default_format_opts = { lsp_format = "fallback" },
-      format_on_save = { timeout_ms = 500, lsp_format = "fallback" },
-      format_after_save = { lsp_format = "fallback" },
+      -- format_on_save = { timeout_ms = 500, lsp_format = "fallback" },
+      -- format_after_save = { lsp_format = "fallback" },
+      format_on_save = nil,
+      format_after_save = nil,
     },
     init = function()
       -- If you want the formatexpr, here is the place to set it
