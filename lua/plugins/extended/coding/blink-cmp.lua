730a09336d702500ec044e18e88a0d5d38fa583d
weitero <112836181+weitero@users.noreply.github.com>
2026-03-25 20:21:30 +0000

    feat: enhance source selection in blink.cmp to prioritize buffer for comment nodes

--- lua/plugins/extended/coding/blink-cmp.lua
+++ lua/plugins/extended/coding/blink-cmp.lua
@@ -23,7 +23,14 @@ return {
       -- Default list of enabled providers defined so that you can extend it
       -- elsewhere in your config, without redefining it, due to `opts_extend`
       sources = {
-        default = { "lsp", "path", "snippets", "buffer" },
+        -- default = { "lsp", "path", "snippets", "buffer" },
+        default = function()
+          local success, node = pcall(vim.treesitter.get_node)
+          if success and node and vim.tbl_contains({ "comment", "line_comment", "block_comment" }, node:type()) then
+            return { "buffer" }
+          end
+          return { "lsp", "path", "snippets", "buffer" }
+        end,
       },
     },
     opts_extend = { "sources.default" },
