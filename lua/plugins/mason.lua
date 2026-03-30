423149f3482b177ff41bfc8b6a5d81abd3a19ad1
weitero <112836181+weitero@users.noreply.github.com>
2026-03-31 00:58:46 +0100

    chore(mason): remove unused plugin configurations
    Cleaned up the mason.lua file by removing plugin
    configurations that are no longer needed, streamlining the setup.

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -13,47 +13,4 @@ return {
       },
     },
   },
-
-  {
-    "mason-org/mason-lspconfig.nvim",
-    opts = {
-      ensure_installed = {
-        "lua_ls",
-      },
-      automatic_enable = {
-        exclude = {
-          "stylua",
-        },
-      },
-    },
-    dependencies = {
-      "mason-org/mason.nvim",
-      "neovim/nvim-lspconfig",
-    },
-    opts_extend = {
-      "ensure_installed",
-      "automatic_enable.exclude",
-    },
-  },
-
-  {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    dependencies = {
-      "mason-org/mason.nvim",
-      "mason-org/mason-lspconfig.nvim",
-    },
-    opts = {
-      ensure_installed = {
-        "stylua",
-      },
-      auto_update = true,
-      integrations = {
-        ["mason-null-ls"] = false,
-        ["mason-nvim-dap"] = false,
-      },
-    },
-    opts_extend = {
-      "ensure_installed",
-    },
-  },
 }
