7233a44ac8549dbcd4c9ee4856a19d37c46ff812
weitero <112836181+weitero@users.noreply.github.com>
2026-03-31 18:03:14 +0100

    feat(lua): move lua language server configuration

--- lua/plugins/lang/lua.lua
+++ lua/plugins/lang/lua.lua
@@ -8,54 +8,7 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        lua_ls = {
-          settings = {
-            -- https://luals.github.io/wiki/settings/
-            Lua = {
-              codeLens = {
-                enable = true,
-              },
-              completion = {
-                callSnippet = "Both",
-                keywordSnippet = "Both",
-              },
-              diagnostics = {
-                disable = { "lowercase-global" },
-                globals = { "vim" },
-                neededFileStatus = {
-                  ["codestyle-check"] = "Any",
-                },
-              },
-              format = {
-                defaultConfig = {
-                  indent_size = "2",
-                  continuation_indent = "2",
-                },
-              },
-              hint = {
-                enable = true,
-              },
-              runtime = {
-                path = {
-                  "?.lua",
-                  "?/init.lua",
-                  "lua/?.lua",
-                  "lua/?/init.lua",
-                },
-                version = "LuaJIT",
-              },
-              telemetry = { enable = false },
-              workspace = {
-                checkThirdParty = "Disable",
-                library = {
-                  "$VIMRUNTIME",
-                  "${3rd}/busted/library",
-                  "${3rd}/luv/library",
-                },
-              },
-            },
-          },
-        },
+        lua_ls = {},
       },
     },
   },
